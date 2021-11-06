c3b8b3ce9acbf3e79d2ed427122a77337d1991d5
JeremyJMoss <mossyj19@gmail.com>
2021-11-06 13:26:36 +1100

    added more GUI funcionality

--- amazon_item_adder.py
+++ amazon_item_adder.py
@@ -1,9 +1,24 @@
 from tkinter import *
+import pandas
+from tkinter import messagebox
 
 
 class AmazonItemAdder:
 
     def __init__(self):
+        try:
+            self.url_entries = pandas.read_csv("url_file.txt")
+        except FileNotFoundError:
+            self.url_entries = {"url": [], "price": []}
+        else:
+            self.url_entries = self.url_entries.to_dict(orient="records")
+            self.url_entries[0].pop("Unnamed: 0")
+            self.url_entries = self.url_entries[0]
+            if isinstance(self.url_entries["url"], str):
+                self.url_entries["url"] = [self.url_entries["url"]]
+                self.url_entries["price"] = [self.url_entries["price"]]
+            else:
+                pass
         self.root = Tk()
         self.root.title("Amazon Price Tracker")
         self.root.config(padx=20, pady=20)
@@ -21,8 +36,30 @@ class AmazonItemAdder:
         self.show_list.grid(row=4, column=0, columnspan=2,  pady=20)
         self.root.mainloop()
 
-    def show_items(self):
+    def check_list_existence(self):
+        try:
+            pandas.read_csv("url_file.txt")
+        except FileNotFoundError:
+            messagebox.showwarning(title="No Url Found", message="No url found for tracking in database")
+            return False
+        else:
+            return True
 
+    def stringify_url_list(self):
+        url_list = pandas.read_csv("url_file.txt")
+        return url_list
+
+    def show_items(self):
+        if self.check_list_existence():
+            window = Tk()
+            window.title("Url List")
 
     def submit_to_form(self):
-        pass
+        url = self.entry_url.get()
+        self.entry_url.delete(0, END)
+        price = f"${self.entry_price.get()}"
+        self.entry_price.delete(0, END)
+        self.url_entries["url"].append(url)
+        self.url_entries["price"].append(price)
+        df = pandas.DataFrame(self.url_entries)
+        df.to_csv("url_file.txt")
